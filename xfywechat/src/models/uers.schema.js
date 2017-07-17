d65560bea3ba62593f0b82fc240f23f6cf2399db
yaoyunchou <303630573@qq.com>
2017-07-17 08:03:38 +0800

    test Schema

--- xfywechat/src/models/uers.schema.js
+++ xfywechat/src/models/uers.schema.js
@@ -3,3 +3,14 @@
  * Created by yao on 2017/6/23.
  */
 import {Schema} from 'mongoose';
+
+export default new Schema({
+    name:{
+        type:Schema.Types.String,
+        unique:true,
+        required:true
+    },
+    nickName:Schema.Types.String,
+    desc:Schema.Types.String,
+    email:String
+})
