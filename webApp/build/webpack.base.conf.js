edecd337a60a5cdadb7837055c10c7b30b6f2d9b
yaoyunchou <303630573@qq.com>
2017-08-10 00:20:06 +0800

    update

--- webApp/build/webpack.base.conf.js
+++ webApp/build/webpack.base.conf.js
@@ -50,6 +50,10 @@ module.exports = {
 					limit: 10000,
 					name: utils.assetsPath('media/[name].[hash:7].[ext]')
 				}
+			}, {
+				test: /\.less$/,
+				loader: "style-loader!css-loader!less-loader",
+				include: []
 			},
 			{
 				test: /\.html$/,
@@ -62,10 +66,6 @@ module.exports = {
 					limit: 10000,
 					name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
 				}
-			}, {
-				test: /\.less$/,
-				loader: "style-loader!css-loader!less-loader",
-				include: ['src']
 			}
 		]
 	}
--- webApp/src/main.js
+++ webApp/src/main.js
@@ -3,12 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'element-ui/lib/theme-default/index.css'
-
 import './style/weui/weui.less'
+import 'element-ui/lib/theme-default/index.css'
 
 
 import ElementUI from 'element-ui'
+
 //resolve => require(['element-ui/lib/theme-default/index.css'], resolve)
 Vue.config.productionTip = false
 Vue.use(ElementUI);
