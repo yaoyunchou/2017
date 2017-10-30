8dfeec6b8094277a31a6ead1876b471c6644edcd
yaoyunchou <303630573@qq.com>
2017-10-30 10:48:34 +0800

    学习笔记上传‘

--- study/javascript/function/20171028/study01.js
+++ study/javascript/function/20171028/study01.js
@@ -1,52 +1,14 @@
 /**
- * Created by yaoyc on 2017/2/9.
- */
-(function(){
-	"use strict";
-	
-	/**
-	 * 定义一个返回参数的函数
-	 * @param v {string}
-	 * @returns {Function}
-	 */
-	function constfunc(v){
-		return function(){
-			return v;
-		};
-	}
-	var func = [];
-	for(var i =0;i<10; i++){
-		func[i] = constfunc(i);
-	}
-	console.log(func[5]());
-	/**
-	 * 扩展对比上面的代码，在同一作用域的时候多个闭包会有什么不同
-	 * @returns {Array}
-	 */
-	function constfuncX(){
-		var func1 = [];
-		for(var i=0;i<10;i++){
-			func1[i] = function (){return i;};
-		}
-		return func1;
-	}
-	var fs = constfuncX();
-	console.info(fs[5]());
-})();
-
-// 20171028
-/**
- * 针对上的学习历史记录发现前面的学习是不全面的，以后要针对什么是闭包，有什么好处，怎么用。这三个方面来进行学习。
- * 
- * 
- * 
+ * 了解函数
+ * 当函数
  * 
  */
 
+ 
  /**
   * 自调用函数
   */
- (function(){
+  (function(){
 	//测试
 
 	console.log(chou(10,20));
@@ -64,7 +26,13 @@
 	var yao = function yun(a,b){
 		return a+b;
 		
-	};
+    };
+    //这个是函数表达式
+    var huang = function (a,b){
+        return a+b;
+    };
+
+    
     //这个是声明式函数表达式，函数声明，
 	function chou(a,b){
 		return a*b;
