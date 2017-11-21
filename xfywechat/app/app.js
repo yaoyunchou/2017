9fc98740f4cd4880ea22ec460727bd01f2b410c2
yaoyunchou <303630573@qq.com>
2017-11-21 09:17:32 +0800

    同步代码‘

--- xfywechat/app/app.js
+++ xfywechat/app/app.js
@@ -78,8 +78,17 @@ mongoose.Promise = global.Promise;
 
 var content = 'mongodb://localhost/yao';
 
-mongoose.connect('mongodb://yao:YaoPwd127899YCC@crm.liangcanl.cn:59832/yao', {
-	//mongoose.connect('mongodb://localhost/yao', {
+// mongoose.connect('mongodb://yao:YaoPwd127899YCC@crm.liangcanl.cn:59832/yao', {
+// 	//mongoose.connect('mongodb://localhost/yao', {
+// 	useMongoClient: true,
+// 	/* other options */
+// 	server: {
+// 		auto_reconnect: true,
+// 		poolSize: 10
+// 	}
+// });
+
+mongoose.connect('mongodb://localhost/yao', {
 	useMongoClient: true,
 	/* other options */
 	server: {
@@ -87,11 +96,6 @@ mongoose.connect('mongodb://yao:YaoPwd127899YCC@crm.liangcanl.cn:59832/yao', {
 		poolSize: 10
 	}
 });
-// mongoose.connect(content, {
-//   //mongoose.connect('mongodb://localhost/yao', {
-//     useMongoClient: true,
-//     /* other options */
-//   });	
 
 //import "./test"
 //console.log(process.env);
